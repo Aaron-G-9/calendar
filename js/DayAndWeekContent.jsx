2f80e3f27087f0831abbfa5c217dd291553b7aa4
Aaron <amgoodfellow@oakland.edu>
2016-06-28 16:45:30 -0400

    Deleted 'All Day' from time array

--- js/DayAndWeekContent.jsx
+++ js/DayAndWeekContent.jsx
@@ -1,6 +1,6 @@
 import React, { PropTypes } from 'react'
 
-const prettyHours = ["All Day", "7am", "8am", "9am", "10am", "11am", "12am", "1pm",
+const prettyHours = ["7am", "8am", "9am", "10am", "11am", "12am", "1pm",
     "2pm", "3pm", "4pm", "5pm", "6pm", "7pm", "8pm", "9pm", "10pm", "11pm"];
 
 export default class DayAndWeek extends React.Component{
@@ -14,7 +14,7 @@ export default class DayAndWeek extends React.Component{
 class DayContent extends React.Component{
   render() {
     var contentboxes = [];
-    for (var i = 0; i < prettyHours.length; i++){
+    for (var i = 0; i < (prettyHours.length * 2); i++){
       contentboxes.push(<HourContent />);
     }
     return (
