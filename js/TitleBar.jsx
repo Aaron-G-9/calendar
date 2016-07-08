0df4b225ab9d99befacb489891aadbbc5f4e10f7
Aaron <amgoodfellow@oakland.edu>
2016-07-08 13:00:40 -0400

    Adding logic to viewChanging buttons

--- js/TitleBar.jsx
+++ js/TitleBar.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-var d = new Date();
+
 const monthNames = ["January", "February", "March", "April", "May", "June",
 "July", "August", "September", "October", "November", "December"
 ];
@@ -9,21 +9,22 @@ const prettyHours = ["All Day", "7am", "8am", "9am", "10am", "11am", "12am", "1p
 
 var sundayWeek = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-var currentMonth = d.getMonth();
-var currentYear = d.getUTCFullYear();
-var currentDay = d.getDate();
-var currentMonthName = monthNames[currentMonth];
-
+var currentMonth;
+var currentYear;
+var currentDay;
 
 
 export default class TitleBar extends React.Component{
   render(){
+    currentMonth = this.props.getDateSelection().selectedMonth;
+    currentYear = this.props.getDateSelection().selectedYear;
+    currentDay = this.props.getDateSelection().selectedDay;
     return (
       <div className="titleBar">
           <button className="scheduleButtons">
             <i className="material-icons">today</i>
           </button>
-          <DatePaginator selectedWeek={this.props.selectedWeek} currentView={this.props.currentView} changeDateSelection={this.props.changeDateSelection} />
+          <DatePaginator getDateSelection={this.props.getDateSelection} selectedWeek={this.props.selectedWeek} currentView={this.props.currentView} changeDateSelection={this.props.changeDateSelection} />
           <ViewSelectorButtons changeDesiredView={this.props.changeDesiredView} />
       </div>
     );
@@ -55,18 +56,32 @@ class ViewSelectorButtons extends React.Component{
   }
 }
 
-
-
 class DatePaginator extends React.Component{
 
-
   forwardMonth(){
-    if (currentMonth < 11){
-      currentMonth += 1;
+    if(this.props.currentView == "MonthView"){
+      if (currentMonth < 11){
+        currentMonth += 1;
+      }else{
+        currentMonth = 0;
+        currentYear +=1;
+      }
+    }else if (this.props.currentView == "WeekView"){
+      var d = new Date();
+      if (d.getWeekOfMonth < this.props.selectedWeek){
+
+
+
+
+            var weeksInCurrentMonth = new Date(2016,12,1).getWeekOfMonth();
+            var weeksInDecember2012 = new Date(2012,12,1).getWeekOfMonth(); // 6
+            alert(weeksInCurrentMonth);
+        }
+
     }else{
-      currentMonth = 0;
-      currentYear +=1;
+      alert("I don't know what to do...");
     }
+
 
     this.props.changeDateSelection(currentMonth, currentYear, currentDay );
   }
@@ -88,7 +103,7 @@ class DatePaginator extends React.Component{
         <button className="scheduleButtons" onClick={this.backMonth.bind(this)}>
           <i className="material-icons">navigate_before</i>
         </button>
-        <DateRange selectedWeek={this.props.selectedWeek} currentView={this.props.currentView} month={monthNames[currentMonth]} year={currentYear}/>
+        <DateRange getDateSelection={this.props.getDateSelection} selectedWeek={this.props.selectedWeek} currentView={this.props.currentView} month={monthNames[this.props.getDateSelection().selectedMonth]} year={this.props.getDateSelection().selectedYear}/>
         <button className="scheduleButtons" id="scheduleForwardWeek" onClick={this.forwardMonth.bind(this)}>
           <i className="material-icons">navigate_next</i>
         </button>
