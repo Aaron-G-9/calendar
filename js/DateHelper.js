877b2becb8f8f184464e57d319fab1257f3fffaa
Aaron <amgoodfellow@oakland.edu>
2016-07-08 10:53:17 -0400

    Added a class that will eventually hold all the date manipulation methods I use

---
+++ js/DateHelper.js
@@ -0,0 +1,54 @@
+export default class DateHelper{
+  static getWeeksOfMonth(day){
+
+    var firstDay = new Date(day.setDate(1)).getDay();
+    var totalDays = new Date(day.getFullYear(), day.getMonth() + 1, 0).getDate();
+    return Math.ceil((firstDay + totalDays) / 7);
+
+  }
+
+  static getWeekArray(month, year, weekNumber){
+    var day = new Date(year, month, 1);
+    var weeksOfMonth = this.getWeeksOfMonth(day);
+    var startDay = day.getDay();
+    var daysInMonth = new Date(year, month + 1, 0);
+    daysInMonth = daysInMonth.getDate();
+    var weekArray = [];
+/*
+    console.log(
+      "Day : " + day +
+      " weeksOfMonth : " + weeksOfMonth +
+      " startDay : " + startDay +
+      " daysInMonth : " + daysInMonth
+    );
+*/
+    if (weekNumber == 1){
+      var dayNumber = 1;
+      for (var i = 0; i < 7; i++){
+        if (startDay > i){
+          weekArray[i] = 0;
+        }else{
+          weekArray[i] = dayNumber;
+          dayNumber++;
+        }
+      }
+      return weekArray;
+    }else if (weekNumber == weeksOfMonth){
+      var endWeekStart = (
+        (7 * weeksOfMonth - 2) + (7 - startDay) + 1
+      );
+      for (var i = 0; i < 7; i++){
+        if (endWeekStart < daysInMonth){
+          weekArray[i] = endWeekStart;
+        }else{
+          weekArray[i] = 0;
+        }
+        endWeekStart++;
+      }
+      return weekArray;
+    }else {
+      return [2, 2, 2, 2, 2];
+    }
+
+  }
+}
