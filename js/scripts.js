223df4bca3df0dad6e17f954f5fd8953119e21b5
Aaron <amgoodfellow@oakland.edu>
2016-07-14 14:42:12 -0400

    Added comments

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,8 @@
+//Necessary import statements for react to work
 import React from "react";
 import ReactDOM from "react-dom";
-
+//Importing our parent component
 import Calendar from "./Calendar.jsx"
-
+//Renders the application
 const app = document.getElementById('app');
 ReactDOM.render(<Calendar />, app);
