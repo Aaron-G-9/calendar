5d15f14400a0ff4790f10edd19cbe164e358de65
Aaron <amgoodfellow@oakland.edu>
2016-07-18 09:24:36 -0400

    Added comments

--- js/DayAndWeekHours.jsx
+++ js/DayAndWeekHours.jsx
@@ -1,18 +1,17 @@
-import React, { PropTypes } from 'react'
+import React from 'react'
 
+//This array might be later moved into DateHelper class
+//This is the text displayed on the left of dayView. It goes from 7-11
+//because those are the times that classes are offered at the school that this
+//is for.
 const prettyHours = ["7am", "8am", "9am", "10am", "11am", "12am", "1pm",
     "2pm", "3pm", "4pm", "5pm", "6pm", "7pm", "8pm", "9pm", "10pm", "11pm"];
 
+//This component displays an array of the child component. This is an especially
+//cool feature that React offers. And we can push however many components we want
+//This array contains as many components as the length of the 'prettyHours array'
+//This will create a div for each element in the array
 export default class DayAndWeekHours extends React.Component{
-  render () {
-    return (
-        <DayHours/>
-    );
-  }
-}
-
-
-class DayHours extends React.Component{
   render() {
     var timeboxes = [];
     for (var i = 0; i < prettyHours.length; i++){
@@ -27,7 +26,9 @@ class DayHours extends React.Component{
   }
 }
 
-
+//This is the child component that DayAndWeekHours makes an array of. All it
+//takes as props is a title, which is just the prettyHours element corresponding
+//to which HourGuideBox you're creating
 class HourGuideBox extends React.Component{
   render() {
     return (
