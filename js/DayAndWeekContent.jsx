0880ef937a81616e54fd04bfc48c2e7465c4deea
Aaron <amgoodfellow@oakland.edu>
2016-07-18 09:54:44 -0400

    prettyHours is no longer a local array. Since several files use it, it's in a getter in DateHelper

--- js/DayAndWeekContent.jsx
+++ js/DayAndWeekContent.jsx
@@ -4,8 +4,7 @@ import OverlayTrigger from 'react-bootstrap/lib/OverlayTrigger';
 import Popover from 'react-bootstrap/lib/Popover';
 import DateHelper from './DateHelper.js';
 
-const prettyHours = ["7am", "8am", "9am", "10am", "11am", "12am", "1pm",
-    "2pm", "3pm", "4pm", "5pm", "6pm", "7pm", "8pm", "9pm", "10pm", "11pm"];
+
 
 var currentMonth;
 var currentYear;
--- js/DayAndWeekHours.jsx
+++ js/DayAndWeekHours.jsx
@@ -1,11 +1,5 @@
 import React from 'react'
 
-//This array might be later moved into DateHelper class
-//This is the text displayed on the left of dayView. It goes from 7-11
-//because those are the times that classes are offered at the school that this
-//is for.
-const prettyHours = ["7am", "8am", "9am", "10am", "11am", "12am", "1pm",
-    "2pm", "3pm", "4pm", "5pm", "6pm", "7pm", "8pm", "9pm", "10pm", "11pm"];
 
 //This component displays an array of the child component. This is an especially
 //cool feature that React offers. And we can push however many components we want
@@ -15,8 +9,8 @@ export default class DayAndWeekHours extends React.Component{
   render() {
     var timeboxes = [];
     for (var i = 0; i < prettyHours.length; i++){
-      timeboxes.push(<HourGuideBox title={prettyHours[i]} />);
-      timeboxes[i] = (<HourGuideBox Box title={prettyHours[i]} />);
+      timeboxes.push(<HourGuideBox title={DateHelper.getprettyHours()[i]} />);
+      timeboxes[i] = (<HourGuideBox Box title={DateHelper.getprettyHours()[i]} />);
     }
     return (
       <div className="dayview">
