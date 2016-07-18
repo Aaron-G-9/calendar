bd2e2978e74e4f37816cfd9444eedeff41cd705b
Aaron <amgoodfellow@oakland.edu>
2016-07-18 13:26:42 -0400

    Beautified code

--- js/Day.jsx
+++ js/Day.jsx
@@ -6,13 +6,13 @@ import DayAndWeekHours from "./DayAndWeekHours.jsx";
 //This component, 'DayGrid' includes both the hours on the left, and then actual
 //column/row view where the events will go. The side hour guide is used for both
 //week and day views (hence the name, DayAndWeekHours)
-export default class DayGrid extends React.Component{
-  render() {
-    return (
-      <div className="daygrid">
-        <DayAndWeekHours />
-        <DayAndWeekContent getDateSelection={this.props.getDateSelection} courseObject={this.props.courseObject} />
-      </div>
-    );
-  }
+export default class DayGrid extends React.Component {
+    render() {
+        return (
+            <div className="daygrid">
+                <DayAndWeekHours/>
+                <DayAndWeekContent getDateSelection={this.props.getDateSelection} courseObject={this.props.courseObject}/>
+            </div>
+        );
+    }
 }
